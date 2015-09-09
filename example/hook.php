e1ba52fbbb8a5c33a98d105c05bac775cf499b89
liangshan <2lisum3@gmail.com>
2015-09-09 12:10:33 +0800

    Add document on example, add new feature into the hook example

--- example/hook.php
+++ example/hook.php
@@ -5,16 +5,21 @@ use ZeroRPC\Hook\ConfigMiddleware;
 use ZeroRPC\Context;
 use ZeroRPC\Client;
 
+/**
+ * This client provided a powful configuration to get zerorpc client.
+ *
+ * Configure use `ZERORPC_` as prefix, that means, if define `ZERORPC_TIME` as key,
+ * then you can get the client by `Client("time")`, it's NOT case sensitive.
+ *
+ * And also support multi-version, for each version of the service, you can either put single
+ * endpoint or an array of endpoints.
+ */
 $middleware = new ConfigMiddleware(array(
     'ZERORPC_TIME' => array(
-        '1.0' => 'tcp://192.168.222.3:8082',
-        'access_key' => 'testing_client_key',
-        'default' => '1.0',
-    ),
-    'ZERORPC_TIME_CLUSTER' => array(
-        '1.0' => array(
-            'tcp://192.168.222.3:8082',
-            'tcp://192.168.222.3:8083',
+        '1.0' => 'tcp://127.0.0.1:1234',
+        '2.0' => array(
+            'tcp://127.0.0.1:2345',
+            'tcp://127.0.0.1:1234',
         ),
         'access_key' => 'testing_client_key',
         'default' => '1.0',
@@ -26,9 +31,7 @@ $context->registerHook('resolve_endpoint', $middleware->resolveEndpoint());
 $context->registerHook('before_send_request', $middleware->beforeSendRequest());
 
 $client = new Client("time", '1.0', $context);
-$client->setTimeout(3500);
-$client->sleep(2);
+print $client->time() . PHP_EOL;
 
-$anotherClient = new Client("time_cluster", '1.0', $context);
-$anotherClient->setTimeout(3500);
-$anotherClient->sleep(2);
+$anotherClient = new Client("time", '2.0', $context);
+print $anotherClient->time() . PHP_EOL;
